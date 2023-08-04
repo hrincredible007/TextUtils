9538be1616bf1c70fdf9987930ddebb57534a30f
hrincredible007 <hritikbhardwaj2002@gmail.com>
2023-08-05 01:09:48 +0530

    Basic TextUtils Complete

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,24 +1,30 @@
 import { React, useState } from "react";
 
 const TextForm = (props) => {
+  const handleUpClick = () => {
+    let temp = text.toUpperCase();
+    setText(temp);
+  };
 
-    const handleUpClick = ()=>{
-        let temp = text.toUpperCase();
-        setText(temp);
-    }
+//   const evaluate_Words = () =>{
+//     if(text.length() === 0){
+//         return 0;
+//     }
+//     return 
+//   };
 
-    const handleLowClick = ()=>{
-        let temp = text.toLowerCase();
-        setText(temp);
-    }
-    const handleOnChange = (event)=>{
-        setText(event.target.value);
-    }
+  const handleLowClick = () => {
+    let temp = text.toLowerCase();
+    setText(temp);
+  };
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
   const [text, setText] = useState("Enter the text here");
   return (
     <div>
       <div className="container my-3">
-        <h3>{props.heading}</h3>    
+        <h3>{props.heading}</h3>
         <div className="my-3">
           <textarea
             className="form-control"
@@ -28,13 +34,27 @@ const TextForm = (props) => {
             rows="8"
           ></textarea>
         </div>
-        <button onClick={handleUpClick} type="button" className="btn btn-primary">
+        <button
+          onClick={handleUpClick}
+          type="button"
+          className="btn btn-primary"
+        >
           Convert to UpperCase
         </button>
-        <button onClick={handleLowClick} type="button" className="btn btn-primary ms-3">
+        <button
+          onClick={handleLowClick}
+          type="button"
+          className="btn btn-primary ms-3"
+        >
           Convert to LowerCase
         </button>
-        
+      </div>
+      <div className="container">
+        <h3>Your text summary</h3>
+        <div>{text.split(" ").length} words and {text.length} characters</div>
+
+        <h3>Preview</h3>
+        <div>{text}</div>
       </div>
     </div>
   );
